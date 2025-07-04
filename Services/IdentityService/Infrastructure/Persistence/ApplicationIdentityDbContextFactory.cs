b45fdeafd9fbcf70e5ee60dbcc7f1641bc97e20d
Chen Li <cl649@students.waikato.ac.nz>
2025-07-04 17:03:51 +1200

    <CI> Adjust the database default configuration into args

--- Services/IdentityService/Infrastructure/Persistence/ApplicationIdentityDbContextFactory.cs
+++ Services/IdentityService/Infrastructure/Persistence/ApplicationIdentityDbContextFactory.cs
@@ -7,11 +7,27 @@ public class ApplicationIdentityDbContextFactory : IDesignTimeDbContextFactory<A
 {
     public ApplicationIdentityDbContext CreateDbContext(string[] args)
     {
-        var config = new ConfigurationBuilder()
-            .SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("appsettings.json")
-            .Build();
-        var connectionString = config.GetConnectionString("DefaultConnection")!;
+        string? connectionString = null;
+        
+        // get the connect from the args first
+        var connArgIndex = Array.IndexOf(args, "--connectionString");
+        if (connArgIndex >= 0 && connArgIndex + 1 < args.Length)
+        {
+            connectionString = args[connArgIndex + 1];
+        }
+
+        if (string.IsNullOrEmpty(connectionString))
+        {
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development";
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile($"appsettings.{environment}.json", optional: true)
+                .AddEnvironmentVariables()
+                .Build();
+            connectionString = config.GetConnectionString("DefaultConnection")!;
+        }
+
         var optionsBuilder = new DbContextOptionsBuilder<ApplicationIdentityDbContext>();
         optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
         return new ApplicationIdentityDbContext(optionsBuilder.Options);
